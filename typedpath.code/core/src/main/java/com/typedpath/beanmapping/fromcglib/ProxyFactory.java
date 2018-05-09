3c50033cbcc534912d0d338be270c1dd758d6329
RobertT.dv2 <robert.todd@hmcts.net>
2018-05-09 10:26:54 +0100

    tidy

--- typedpath.code/core/src/main/java/com/typedpath/beanmapping/fromcglib/ProxyFactory.java
+++ typedpath.code/core/src/main/java/com/typedpath/beanmapping/fromcglib/ProxyFactory.java
@@ -1,13 +1,13 @@
 package com.typedpath.beanmapping.fromcglib;
 
 import com.typedpath.beanmapping.TypedPath;
-import net.sf.cglib.proxy.Enhancer;
 
 import java.util.function.Function;
 import java.util.function.Predicate;
 
 interface ProxyFactory {
 
+    //TODO - conisder merging these 2 and combine with Handler registration
     Handler createHandler(TypedPath parent, Handler parentHandler, Class itemType,
                           Predicate<Class> isSimple, Function<Class, Object> uniqueSimpleValueDefaulter);
     <S> S create(Class<S> theClass, Handler handler);
